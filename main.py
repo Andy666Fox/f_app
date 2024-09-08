97d8a42b84d894c60e5323f26afe20a2e12ca5da
Andy666Fox <arsale3005@gmail.com>
2024-09-08 10:05:58 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from  fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title='MyApp'
+)
 
-@app.get('/')
-def hello():
-    return {'data': 'Hello world!'}
+fake_users = [
+    {'id': 1, 'role': 'admin', 'name': 'John'}
+]
+
+@app.get('/user/{user_id}')
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get('id')==user_id]
+
