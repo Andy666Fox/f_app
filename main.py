8fbc95a76214bf350226f26dd047190563fecd9c
Andy666Fox <arsale3005@gmail.com>
2024-09-13 09:24:14 +0300

    minor changes

--- main.py
+++ main.py
@@ -33,21 +33,13 @@ app.include_router(
     tags=['auth']
 )
 
+
 app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix='/auth',
     tags=['auth']
 )
 
-current_user = fastapi_users.current_user()
-
-@app.get('/protected-route')
-def protected_route(user: User = Depends(current_user)):
-    return f'Hello, {user.email}'
-
-@app.get('/unprotected-route')
-def unprotected_route():
-    return f'Hello, baby'
 
 
 
